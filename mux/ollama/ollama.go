d1cba9bd09c470ac4c93d4294f277b6f7c4f26f6
yylt <yang8518296@163.com>
2024-09-10 16:35:41 +0800

    fix ollama backend

--- mux/ollama/ollama.go
+++ mux/ollama/ollama.go
@@ -2,21 +2,38 @@ package ollama
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
 	"net/http"
 	"net/url"
 	"sync"
 
+	"github.com/ollama/ollama/api"
 	"github.com/tmc/langchaingo/llms"
-	"github.com/tmc/langchaingo/llms/ollama"
+	"github.com/yylt/gptmux/mux"
 	"github.com/yylt/gptmux/pkg"
+	"k8s.io/klog/v2"
+)
+
+const maxBufferSize = 512 * 1024
+
+var (
+	headers = map[string]string{
+		"accept":       "*/*",
+		"content-type": "application/json",
+	}
+	defaultClient = &http.Client{
+		Transport: http.DefaultTransport,
+	}
 )
 
 type Config struct {
 	Model  string `yaml:"model_name"`
 	Server string `yaml:"server"`
 	Index  int    `yaml:"index,omitempty"`
+}
+type ollamaResp struct {
+	Resp string `yaml:"response,omitempty"`
+	Done bool   `yaml:"done,omitempty"`
 }
 
 type ollm struct {
@@ -26,38 +43,26 @@ type ollm struct {
 
 	index int
 
-	llm *ollama.LLM
+	c *Config
+
+	cli *api.Client
 }
 
 func New(ctx context.Context, cfg *Config) *ollm {
 	if cfg == nil {
 		return nil
 	}
-	var (
-		opts []ollama.Option
-	)
-	opts = append(opts, ollama.WithModel(cfg.Model), ollama.WithServerURL(cfg.Server))
-
 	u, err := url.Parse(cfg.Server)
 	if err != nil {
-		panic(err)
+		klog.Errorf("ollama server failed: %s", err)
+		return nil
 	}
 
-	if u.Scheme == "https" {
-		opts = append(opts, ollama.WithHTTPClient(
-			&http.Client{Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			}},
-		))
-	}
-	llm, err := ollama.New(opts...)
-	if err != nil {
-		panic(err)
-	}
 	return &ollm{
 		ctx:   ctx,
-		llm:   llm,
+		c:     cfg,
 		index: cfg.Index,
+		cli:   api.NewClient(u, defaultClient),
 	}
 }
 
@@ -74,7 +79,47 @@ func (d *ollm) GenerateContent(ctx context.Context, messages []llms.MessageConte
 		return nil, pkg.BusyErr
 	}
 	defer d.mu.Unlock()
-	return d.llm.GenerateContent(ctx, messages, options...)
+	prompt, model := mux.GeneraPrompt(messages)
+
+	if model != mux.TxtModel {
+		return nil, fmt.Errorf("not support model '%s'", model)
+	}
+	var (
+		opt          = &llms.CallOptions{}
+		bctx, cancle = context.WithCancel(ctx)
+		data         = &llms.ContentResponse{}
+
+		once sync.Once
+	)
+
+	for _, o := range options {
+		o(opt)
+	}
+
+	d.cli.Generate(bctx, &api.GenerateRequest{
+		Model:  d.c.Model,
+		Prompt: prompt,
+	}, func(gr api.GenerateResponse) error {
+		data.Choices = append(data.Choices, &llms.ContentChoice{
+			Content: gr.Response,
+		})
+		if gr.Done {
+			data.Choices = append(data.Choices, &llms.ContentChoice{
+				StopReason: "stop",
+			})
+			once.Do(cancle)
+		}
+		if opt.StreamingFunc != nil {
+			err := opt.StreamingFunc(bctx, []byte(gr.Response))
+			if err != nil {
+				once.Do(cancle)
+				return err
+			}
+		}
+		return nil
+	})
+
+	return data, nil
 }
 
 func (d *ollm) Call(ctx context.Context, prompt string, options ...llms.CallOption) (string, error) {
